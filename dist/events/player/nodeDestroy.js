2a45ed708d4e1bd015ce4dd756ba04a3740d98ba
Staz131 <139014413+stazmeijr@users.noreply.github.com>
2025-07-24 01:11:18 +0700

    Delete dist directory

--- dist/events/player/nodeDestroy.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger_1 = require("../../utils/logger");
-exports.default = {
-    name: "nodeDestroy",
-    execute(client, node) {
-        logger_1.logger.warn(`[Riffy] ⚠️ Node "${node.name}" has been destroyed.`);
-    },
-};
